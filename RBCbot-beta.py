2be740c63851f8c16509632e1f3cfbadb1ff1c5c
Loganthedominator <130767938+Loganthedominator@users.noreply.github.com>
2023-04-14 11:05:52 -0400

    Create RBCbot-beta.py

---
+++ RBCbot-beta.py
@@ -0,0 +1,13 @@
+#Version 0.1 (beta)
+
+import time
+import random
+
+print('Hello! My name is RBCbot.')
+time.sleep(1.5)
+print('What\'s your name?')
+print()
+time.sleep(1.5)
+name = input()
+time.sleep(1.5)
+print('Hello ' + name + ', it\'s awesome to meet you!')
